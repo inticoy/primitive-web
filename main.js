255a3fb826572b687c244b4f68768277604e20ba
inticoy <inticoy@hanyang.ac.kr>
2021-05-09 19:09:10 +0900

    comparison

--- main.js
+++ main.js
@@ -17,39 +17,39 @@ var app = http.createServer(function(request,response){
       return;
     }
     response.writeHead(200);
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>${title} practice</title>
-      <mata charset = "utf-8">
-      <link rel ="stylesheet" href="style.css">
-    </head>
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, data){
+      var description = data;
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>${title} practice</title>
+        <mata charset = "utf-8">
+        <link rel ="stylesheet" href="style.css">
+      </head>
 
-    <body>
-      <h1> <a href="/">WEB</a> </h1>
-      <div id="grid">
-        <ol>
-          <li><a href="/?id=HTML"class="saw" id="active">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <div id="article">
-          <h2>${title}</h2>
+      <body>
+        <h1> <a href="/">WEB</a> </h1>
+        <div id="grid">
+          <ol>
+            <li><a href="/?id=HTML"class="saw" id="active">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <div id="article">
+            <h2>${title}</h2>
+            <p>${description}</p>
 
-          <p>
-            <a href="https://www.w3.org/TR/html52/" target="_blank" title="html5 specific">Hypertext Markup Language (HTML)</a>
-            is the standard markup language for
-          <strong> creating <u>web</u> pages </strong>
-          and web applications.
-          </p>
-          <img width="500px" src="test.jpg">
+            <img width="500px" src="test.jpg">
+          </div>
         </div>
-      </div>
-    </body>
+      </body>
+      `;
+      response.end(template);
 
-    `;
-    response.end(template);
+    })
+
+
 
 });
 app.listen(3000);
